5907ea7c19b96129d5252ef6a404d5e3265071ed
Eivind Lie Andreassen <ela4991@gmail.com>
2020-05-11 14:59:06 +0200

    WIP on interactive mode

--- plot_file/__init__.py
+++ plot_file/__init__.py
@@ -1,13 +1,59 @@
 import argparse
+from types import SimpleNamespace
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
+def get_args_interactive():
+    path = None
+    while not path:
+        path = input('Path to data file: ')
+    
+    delimiter, index_col = None, None
+    if path.endswith('.csv') or path.endswith('.tsv') or path.endswith('.txt'):
+        delimiter = input('Delimiter in the data file (leave blank for ","): ')
+        delimiter = delimiter if delimiter else ','
+
+        index_col = input('Index column of the data file (header or index). Leave blank to generate index: ')
+        index_col = index_col if index_col else None
+
+    columns = input('Columns to be plotted (headers or indices, separated by comma): ')
+    columns = [x.strip() for x in columns.split(',')]
+
+    plot_type = input('Plot type ["line", "bar", "barh", "hist", "box", "area"]: ')
+    plot_type = plot_type if plot_type else 'line'
+
+    output_file = input('Output file (leave blank to not save plot): ')
+    title = input('Plot title: ')
+    title = title if title else None
+    xlabel = input('x-axis label: ')
+    xlabel = xlabel if xlabel else None
+    ylabel = input('y-axis label: ')
+    ylabel = ylabel if ylabel else None
+
+    args = {
+        'path': path,
+        'delimiter': delimiter,
+        'index_col': index_col,
+        'columns': columns,
+        'type': plot_type,
+        'output_file': output_file,
+        'title': title,
+        'xlabel': xlabel,
+        'ylabel': ylabel,
+        'header': None,
+    }
+
+    return SimpleNamespace(**args)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Quickly visualize data from a text file.')
     parser.add_argument('path', type=str, help='Path to the file that should be plotted.', nargs='?', default=None)
+    parser.add_argument('--interactive', '-i', dest='interactive', action='store_true',
+                        help='Run in interactive mode (Enter arguments one at a time).')
     parser.add_argument('--columns', '-c', dest='columns', help='The column(s) that should be plotted, either as column index or,' +  
                                                                 'if the file contains headers, column name.',
                         default=None, nargs='+')
@@ -27,7 +73,9 @@ def parse_args():
 
     args = parser.parse_args()
 
-    if args.path is None:
+    if args.interactive:
+        args = get_args_interactive()
+    elif args.path is None:
         parser.print_help()
         return None
 
@@ -40,9 +88,9 @@ def load_data(path, delimiter=',', header=None, index_col=None):
     except (ValueError, TypeError):
         pass
 
-    if path.endswith('.csv') or path.endswith('.tsv') or path.endswith('.txt')
+    if path.endswith('.csv') or path.endswith('.tsv') or path.endswith('.txt'):
         data = pd.read_csv(path, delimiter=delimiter, header=0 if header else None, index_col=index_col)
-        data = data.rename(columns=lambda x: x.strip())
+        data = data.rename(columns=lambda x: x.strip() if isinstance(x, str) else x)
     elif path.endswith('.h5'):
         data = pd.read_hdf(path)
     else:
