218cd4e22d70a22e8076e86f63b6aa650f4c488c
Eivind Lie Andreassen <ela4991@gmail.com>
2020-05-11 12:06:55 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version_string = '0.0.3'
+
+description = 'A script for quickly plotting data from a text file.'
+
+setup(
+    name='plot_file',
+    version=version_string,
+    packages=find_packages(),
+
+    install_requires=[
+        "pandas>=1.0.3",
+        "matplotlib>=3.2.1",
+        "numpy>=1.18.4"
+    ],
+
+    author='Eivind Lie Andreassen',
+    description=description,
+    long_description=description,
+
+    entry_points={
+        "console_scripts": [
+            "plot_file = plot_file:main"
+        ]
+    }
+    
+)
